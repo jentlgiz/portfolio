27da1876ea6c644fb153ddf280abfb0b0e02f66c
Nargiz S <sabitova.nargiz@yandex.ru>
2025-10-04 19:01:36 +0500

    feat: add media bar navigation on click

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,15 +1,16 @@
 // closing and opening burger menu
 
-let mediaNavButton = document.querySelector('.media-nav-button');
-let mediaNavButtonLine = document.querySelector('.burger-button__line');
-let mediaOverlay = document.querySelector('.media-nav__overlay');
+const mediaNavButton = document.querySelector('.media-nav-button');
+const mediaNavButtonLine = document.querySelector('.burger-button__line');
+const mediaOverlay = document.querySelector('.media-nav__overlay');
+const mediaBar = document.querySelector('.media-nav__bar');
 
 // activating mobile navigation
 mediaNavButton.addEventListener('click', function () {
     // click on line
     mediaNavButtonLine.classList.toggle('active');
-    //overlay active
+    // overlay active
     mediaOverlay.classList.toggle('active');
-
-
+    // media navigation bar activation
+    mediaBar.classList.toggle('active');
 });
