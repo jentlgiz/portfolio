b654e80e53a35effdf9a7b5d0c4198dfb9b4d30c
Nargiz S <sabitova.nargiz@yandex.ru>
2025-10-04 18:27:09 +0500

    feat: add overlay for media navigation

--- portfolio/main.js
+++ portfolio/main.js
@@ -2,10 +2,14 @@
 
 let mediaNavButton = document.querySelector('.media-nav-button');
 let mediaNavButtonLine = document.querySelector('.burger-button__line');
+let mediaOverlay = document.querySelector('.media-nav__overlay');
 
 // activating mobile navigation
 mediaNavButton.addEventListener('click', function () {
     // click on line
     mediaNavButtonLine.classList.toggle('active');
+    //overlay active
+    mediaOverlay.classList.toggle('active');
+
 
 });
