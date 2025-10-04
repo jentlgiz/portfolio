cb82bf8bd068f45a64911b08b58d67eba5b3b375
Nargiz S <sabitova.nargiz@yandex.ru>
2025-10-04 19:17:48 +0500

    fix: add turn off media bar on click around whole navigation (including overlay)

--- portfolio/main.js
+++ portfolio/main.js
@@ -4,6 +4,7 @@ const mediaNavButton = document.querySelector('.media-nav-button');
 const mediaNavButtonLine = document.querySelector('.burger-button__line');
 const mediaOverlay = document.querySelector('.media-nav__overlay');
 const mediaBar = document.querySelector('.media-nav__bar');
+const mediaBarNav = document.querySelector('.media-nav__bar-content');
 
 // activating mobile navigation
 mediaNavButton.addEventListener('click', function () {
@@ -14,3 +15,32 @@ mediaNavButton.addEventListener('click', function () {
     // media navigation bar activation
     mediaBar.classList.toggle('active');
 });
+
+    // click on overlay around navigation
+    mediaOverlay.addEventListener('click', function () {
+        turnOffMediaNav()
+    });
+
+    mediaBarNav .addEventListener('click', function () {
+        turnOffMediaNav()
+    });
+
+function turnOffMediaNav() {
+        // turn off a burger button
+        if (mediaNavButtonLine.classList.contains('active')) {
+            mediaNavButtonLine.classList.remove('active');
+        }
+
+        // turn off overlay
+        if (mediaOverlay.classList.contains('active')){
+            mediaOverlay.classList.remove('active');
+        }
+
+        // turn off media navigation bar
+        if (mediaBar.classList.contains('active')){
+            mediaBar.classList.remove('active');
+        }
+};
+
+
+
