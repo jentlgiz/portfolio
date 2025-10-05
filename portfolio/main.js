47798fed9e83fbf0c0d6edbcc62bc28f43b9f36a
Nargiz S <sabitova.nargiz@yandex.ru>
2025-10-05 17:17:54 +0500

    feat: add modal window functionality

--- portfolio/main.js
+++ portfolio/main.js
@@ -76,20 +76,28 @@ accordionBoxHeader.forEach(function (item) {
 // modal
 
 const modalButton = document.querySelector('[data-modal]');
-// console.log(modalButton);
+const modalButtonWindow = document.querySelector('[data-modal-button]');
+const modalCloseButton = document.querySelector('[data-close]');
 
 modalButton.addEventListener('click', function () {
-    // console.log('Click');
-
-    const modalButtonWindow = document.querySelector('[data-modal-button]');
     modalButtonWindow.classList.remove('price-card__modal-active');
-
-    const modalCloseButton = document.querySelector('[data-close]');
-    modalCloseButton.addEventListener('click', function () {
-        console.log('Click works');
-    })
 })
 
+modalCloseButton.addEventListener('click', function () {
+    modalButtonWindow.classList.add('price-card__modal-active');
+})
+
+modalButtonWindow.addEventListener('click', function () {
+    console.log('Close Modal');
+    modalButtonWindow.classList.add('price-card__modal-active');
+
+})
+
+// modal - invisible to clicks inside window
+
+modalButtonWindow.querySelector('.price-card__modal-window').addEventListener('click', function (event) {
+    event.stopPropagation();
+})
 
 
 
